07264c6ac151410385559614322229c82e3af263
Alba Gustems <albagustems@gmail.com>
2021-01-10 19:47:28 +0100

    Text and readme small changes

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -4,51 +4,54 @@ import {Injectable} from '@angular/core';
 export class HeroesService {
     private heroes : Hero[] = [
         {
-            nombre: "Aquaman",
-            bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse c" +
-                    "on la vida marina, la cual puede convocar a grandes distancias.",
+            name: "Aquaman",
+            bio: "A half-Atlantean, half-human who is reluctant to be king of the undersea nation " +
+                    "of Atlantis. He possesses superhuman strength and the ability to manipulate the " +
+                    "tides, communicate with sea creatures and swim at supersonic speeds.",
             img: "assets/img/aquaman.png",
-            aparicion: "1941-11-01",
-            casa: "DC"
+            appearance: "1941-11-01",
+            house: "DC"
         }, {
-            nombre: "Batman",
-            bio: "Los rasgos principales de Batman se resumen en «destreza física, habilidades ded" +
-                    "uctivas y obsesión». La mayor parte de las características básicas de los cómics" +
-                    " han variado por las diferentes interpretaciones que le han dado al personaje.",
+            name: "Batman",
+            bio: "Batman has been Gotham City's protector for decades, CEO of Wayne Enterprises, P" +
+                    "atriarch of the Bat Family and veteran member of the Justice League. Batman is t" +
+                    "he secret identity of Bruce Wayne. Witnessing the murder of his parents as a chi" +
+                    "ld leads him to train himself to physical and intellectual perfection and don a " +
+                    "bat-themed costume in order to fight crime. Batman operates in the Batcave, bene" +
+                    "ath Wayne Manor assisted by his butler Alfred Pennyworth",
             img: "assets/img/batman.png",
-            aparicion: "1939-05-01",
-            casa: "DC"
+            appearance: "1939-05-01",
+            house: "DC"
         }, {
-            nombre: "Daredevil",
-            bio: "Al haber perdido la vista, los cuatro sentidos restantes de Daredevil fueron aum" +
-                    "entados por la radiación a niveles superhumanos, en el accidente que tuvo cuando" +
-                    " era niño. A pesar de su ceguera, puede \"ver\" a través de un \"sexto sentido\"" +
-                    " que le sirve como un radar similar al de los murciélagos.",
+            name: "Daredevil",
+            bio: "Blinded in a childhood accident, attorney-at-law Matt Murdock protects Hell's Ki" +
+                    "tchen as the masked vigilante, Daredevil. As a boy, Matt Murdock lost his sight—" +
+                    "and gained superhuman senses—in an accident that taught him to live without fear" +
+                    ".",
             img: "assets/img/daredevil.png",
-            aparicion: "1964-01-01",
-            casa: "Marvel"
+            appearance: "1964-01-01",
+            house: "Marvel"
         }, {
-            nombre: "Hulk",
-            bio: "Su principal poder es su capacidad de aumentar su fuerza hasta niveles prácticam" +
-                    "ente ilimitados a la vez que aumenta su furia. Dependiendo de qué personalidad d" +
-                    "e Hulk esté al mando en ese momento (el Hulk Banner es el más débil, pero lo com" +
-                    "pensa con su inteligencia).",
+            name: "Hulk",
+            bio: "When Bruce Banner changes into the Hulk, he becomes an unstoppable beast of near" +
+                    " unlimited strength, power, and destruction. The Hulk’s strength is probably the" +
+                    " greatest in the Marvel universe, with many foes falling to his thunderous attac" +
+                    "ks. The Hulk is also able to leap great distances traveling for miles before bou" +
+                    "nding upwards again.",
             img: "assets/img/hulk.png",
-            aparicion: "1962-05-01",
-            casa: "Marvel"
+            appearance: "1962-05-01",
+            house: "Marvel"
         }, {
-            nombre: "Linterna Verde",
-            bio: "Poseedor del anillo de poder que posee la capacidad de crear manifestaciones de " +
-                    "luz sólida mediante la utilización del pensamiento. Es alimentado por la Llama V" +
-                    "erde (revisada por escritores posteriores como un poder místico llamado Starhear" +
-                    "t), una llama mágica contenida en dentro de un orbe (el orbe era en realidad un " +
-                    "meteorito verde de metal que cayó a la Tierra, el cual encontró un fabricante de" +
-                    " lámparas llamado Chang)",
+            name: "Green Lantern",
+            bio: "The Green Lantern name refers to multiple DC super heroes that all gained their " +
+                    "powers from an alien ring. The ring grants the wearer the ability to create what" +
+                    "ever construct they can imagine in the form of light energy along with the power" +
+                    " of flight and a protective aura.",
             img: "assets/img/linterna-verde.png",
-            aparicion: "1940-06-01",
-            casa: "DC"
+            appearance: "1940-06-01",
+            house: "DC"
         }, {
-            nombre: "Spider-Man",
+            name: "Spider-Man",
             bio: "Tras ser mordido por una araña radiactiva, obtuvo los siguientes poderes sobrehu" +
                     "manos, una gran fuerza, agilidad, poder trepar por paredes. La fuerza de Spider-" +
                     "Man le permite levantar 10 toneladas o más. Gracias a esta gran fuerza Spider-Ma" +
@@ -56,18 +59,18 @@ export class HeroesService {
                     "r si un peligro se cierne sobre él, antes de que suceda. En ocasiones este puede" +
                     " llevar a Spider-Man al origen del peligro.",
             img: "assets/img/spiderman.png",
-            aparicion: "1962-08-01",
-            casa: "Marvel"
+            appearance: "1962-08-01",
+            house: "Marvel"
         }, {
-            nombre: "Wolverine",
+            name: "Wolverine",
             bio: "En el universo ficticio de Marvel, Wolverine posee poderes regenerativos que pue" +
                     "den curar cualquier herida, por mortal que ésta sea, además ese mismo poder hace" +
                     " que sea inmune a cualquier enfermedad existente en la Tierra y algunas extrater" +
                     "restres . Posee también una fuerza sobrehumana, que si bien no se compara con la" +
                     " de otros superhéroes como Hulk, sí sobrepasa la de cualquier humano.",
             img: "assets/img/wolverine.png",
-            aparicion: "1974-11-01",
-            casa: "Marvel"
+            appearance: "1974-11-01",
+            house: "Marvel"
         }
     ];
 
@@ -75,15 +78,15 @@ export class HeroesService {
         console.log('Service ready to go!')
     }
 
-    public getHeroes(): Hero[] {
+    public getHeroes() : Hero[] {
         return this.heroes
     }
 }
 
 export interface Hero {
-    nombre: string,
-    bio: string,
-    img: string,
-    aparicion: string,
-    casa: string
+    name : string,
+    bio : string,
+    img : string,
+    appearance : string,
+    house : string
 }
